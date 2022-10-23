4c6f906f9b5f1bb4168df0a07e72ee0c5ff7cd6a
Clementine <clemontine.car@gmail.com>
2022-10-23 20:57:49 +0900

    HTML restructuring

---
+++ index.js
@@ -0,0 +1,12 @@
+// On click sur la galerie de vêtements, la src de l'image change en path de l'image 
+// qui a été cliquée
+setTimeout(()=> {
+    let top = document.querySelector("#top");
+    console.log(top)
+    
+    const tops = document.querySelector("div.tops").children;
+    console.log(tops)
+}, 1
+)
+
+
